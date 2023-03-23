6ed9de810ff20ef9075ddd1abcc305efbdf6f807
maccarets <maccarets@gmail.com>
2023-03-23 14:34:14 +0200

    And models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=False)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=False)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position)
+
+
+class Task(models.Model):
+    HIGH = "high"
+    MEDIUM = "medium"
+    LOW = "low"
+
+    PRIORITIES = (
+        (HIGH, "High"),
+        (MEDIUM, "Medium"),
+        (LOW, "Low")
+    )
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=True, blank=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=8, choices=PRIORITIES, default=MEDIUM, null=False)
+    task_type = models.ForeignKey(TaskType)
+    assignees = models.ManyToManyField(Worker)
+
