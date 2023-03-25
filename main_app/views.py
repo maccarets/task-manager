0350fa9366fba68eb403eeba68e28aea6a9c1544
maccarets <maccarets@gmail.com>
2023-03-25 12:51:58 +0200

    Add TasksListView

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+
+from main_app.models import Task
 
 
 def index(request):
     return render(request, "main_app/index.html")
 
+
+class TasksListView(LoginRequiredMixin, generic.ListView):
+    model = Task
 
 
