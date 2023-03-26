6e7e0d6a9759adacea2fed8d412b391e1dcf8048
maccarets <maccarets@gmail.com>
2023-03-26 15:44:18 +0300

    Add worker_list.html, update sidebar.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,8 @@ from main_app.views import index, TasksListView, WorkersListView
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tasks", TasksListView.as_view(), name="tasks-list"),
-    path("workers", WorkersListView.as_view(), name="workers-list")
+    path("tasks", TasksListView.as_view(), name="task-list"),
+    path("workers", WorkersListView.as_view(), name="worker-list")
 ]
 
 app_name = "main_app"
