79a28b9819d2ea9fc69405845e7c36335f9cd9b4
maccarets <maccarets@gmail.com>
2023-03-26 14:51:05 +0300

    Add WorkersListViev

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
-from main_app.models import Task
+from main_app.models import Task, Worker
 
 
 def index(request):
@@ -13,3 +13,5 @@ class TasksListView(LoginRequiredMixin, generic.ListView):
     model = Task
 
 
+class WorkersListView(LoginRequiredMixin, generic.ListView):
+    model = Worker
