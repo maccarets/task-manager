6d3bb3ffaac0aeef3282ae60d345b71a5ab7bc3a
maccarets <maccarets@gmail.com>
2023-03-26 15:14:06 +0300

    Add task_list.html, update base.html, urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from main_app.views import index
+from main_app.views import index, TasksListView, WorkersListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tasks", TasksListView.as_view(), name="tasks-list"),
+    path("workers", WorkersListView.as_view(), name="workers-list")
 ]
 
 app_name = "main_app"
